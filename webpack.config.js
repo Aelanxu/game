259539c59050969f691dd13d3eafb367b6d9da7a
Aelanxu <aelanxu@outlook.com>
2020-12-10 22:45:42 +0800

    1210

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'game.js',
+
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            // {
+            //     test: [/\.vert$/, /\.frag$/],
+            //     use: "raw-loader"
+            // },
+            {
+                test: /\.(gif|png|jpe?g|svg|xml)$/i,
+                use: "file-loader"
+            }
+        ]
 
     },
     devServer: {
@@ -14,8 +34,18 @@ module.exports = {
         open: true
 
     },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'MyGame',
-        template: 'index.html'
-    })]
+    plugins: [
+
+        new CleanWebpackPlugin({
+            root: path.resolve(__dirname, "../")
+        }),
+        new webpack.DefinePlugin({
+            CANVAS_RENDERER: JSON.stringify(true),
+            WEBGL_RENDERER: JSON.stringify(true)
+        }),
+        new HtmlWebpackPlugin({
+            title: 'MyGame',
+            template: 'index.html'
+        }),
+    ]
 };
